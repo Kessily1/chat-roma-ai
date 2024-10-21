09a27128f9323f9b21ed5459a2147f2f6737465e
Eduardo Cadiz <181763279+educadiz@users.noreply.github.com>
2024-10-21 23:56:35 +0000

    Ajustes no codigo: remocao de linhas de comentario desnecessarias e identação.

--- socketio.js
+++ socketio.js
@@ -1,46 +1,3 @@
-/*
-const express = require('express')
-const httpt = require('http')
-const socketio = require('socket.io')
-
-const app = express()
-const server = httpt.createServer(app)
-const io = socketio(server)
-
-let usuariosOnline = 0; // Contador de usuários online 
-
-app.use(express.static(__dirname));
-
-io.on('connection', (socket) => {
-  console.log('usuario conectado' + socket.id)
-  usuariosOnline++;
-  io.emit('usuariosOnline', usuariosOnline); 
-  io.emit('userStatus', `${socket.id} entrou no chat`); // Notifica todos sobre a entrada
-
-  socket.on('message', (msg) => {
-    console.log (msg);
-    io.emit('message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Usuário desconectado: ' + socket.id);
-    usuariosOnline--;
-    io.emit('usuariosOnline', usuariosOnline);
-    io.emit('userStatus', `${socket.id} saiu do chat`); // Notifica todos sobre a saída
-  });
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/chat.html")
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
-});
-
-//aqui vai o socketio
-
-server.listen(3000) */
 
 const express = require('express');
 const http = require('http');
@@ -59,7 +16,7 @@ io.on('connection', (socket) => {
     console.log('Usuário conectado: ' + socket.id);
 
     // Quando o usuário se conecta, ele deve enviar seu nome
-    socket.on('setUsername', (user) => { // Usei 'user' em vez de 'username'
+    socket.on('setUsername', (user) => { 
         if (!user || Object.values(usuarios).includes(user)) {
             socket.emit('userStatus', 'Nome de usuário inválido ou já em uso.');
             return;
