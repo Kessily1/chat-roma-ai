02be083980ed91ae6d37f5f2e4b8a360ebc3c901
Eduardo Cadiz <181763279+educadiz@users.noreply.github.com>
2024-10-30 21:10:25 +0000

    alterado endereço de sockt para testes locais

--- front/chat.js
+++ front/chat.js
@@ -1,6 +1,6 @@
 // * Atenção: Para testes verifique seu Link e insira no SOCKET abaixo:
 //
-const socket = io("https://neglected-crematorium-pjpj45rpqwj7c9gw-3000.app.github.dev/");
+const socket = io("https://nightmarish-mummy-jjqjvp5qw7693x4r-3000.app.github.dev/");
 //
 //
 
