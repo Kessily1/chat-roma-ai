fe10c02c39f5537bbdb4779586458a33e0b63f25
leonardoaandradee <leonardo.lima@novaroma.edu.br>
2024-11-08 21:47:01 +0000

    Excui o popup.js

--- front/js/popup.js
+++
@@ -1,7 +0,0 @@
-// // Mostrar alerta para notificação de status
-// function mostrarAlerta(mensagem) {
-//     alert(mensagem);  // Exibe a mensagem em uma caixa de alerta
-// }
-
-// // Evento para mostrar alerta quando um usuário entra/sai
-// socket.on('userStatus', mostrarAlerta);
